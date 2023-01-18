131955a488bc1b5d90783a81d95bcf462f0c83fb
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-01-18 20:12:17 +0900

    [Bronze V] Title: N 찍기, Time: 1020 ms, Memory: 31656 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 1; i <= t; i++) 
+			System.out.println(i);
+		
+		
+}
+}
