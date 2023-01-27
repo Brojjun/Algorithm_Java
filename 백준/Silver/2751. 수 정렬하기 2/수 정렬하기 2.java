d6c9ddd54205381a85948f69950170e8e0188d4b
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-01-28 02:05:44 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1084 ms, Memory: 143684 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int map[];
+	static int virtual[];
+	static int bn;
+
+	public static void input() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		bn = Integer.parseInt(br.readLine());
+		map = new int[bn];
+		virtual = new int[bn];
+
+		for (int i = 0; i < bn; i++)
+			map[i] = Integer.parseInt(br.readLine());
+
+	}
+
+	public static void sort(int begin, int end) {
+		if (begin == end)
+			return;
+
+		int mid = (begin + end) / 2;
+		sort(begin, mid);
+		sort(mid + 1, end);
+		merge(begin, end);
+
+	}
+
+	public static void print() throws IOException {
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		char[] rs = new char[bn * 2];
+		
+		for (int i = 0; i < bn; i++)
+			sb.append(map[i] + "\n");
+		
+			System.out.print(sb);
+	}
+	public static void merge(int begin, int end) {
+		int mid = (begin + end) / 2;
+
+		int left = begin; // 왼쪽 시작
+		int right = mid + 1; // 오른 쪽 시작
+		int cnt = begin; // 현재위치
+
+		// 한쪽이 끝에 도달하면 정지
+		while (left <= mid && right <= end) {
+			if (map[left] <= map[right])
+				virtual[cnt++] = map[left++];
+			else
+				virtual[cnt++] = map[right++];
+		}
+
+		// 나머지 채워주기
+		if (left > mid) {
+				while(right <= end)
+					virtual[cnt++] = map[right++];
+		} 
+		
+		else {
+			while(left <= mid)
+				virtual[cnt++] = map[left++];
+		}
+		
+		
+		//변화를 map에 반영해주기
+		for(int i = begin; i <= end; i++) map[i] = virtual[i];
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+		sort(0, bn - 1); // 인자값은 map배열의 시작 위치와 끝 위치
+		print();
+
+
+	}
+}
