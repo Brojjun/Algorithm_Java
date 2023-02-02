e3f5a9c4741cf14652b3e1ca844f05134d55b6fc
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-02 20:54:37 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 316 ms, Memory: 19768 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	
+		while (true) {
+			boolean check = false;
+	
+			Stack<Character> arr = new Stack<>();
+			
+			String st = br.readLine();
+			
+			if(st.equals("."))return;
+			
+			for (int i = 0; i < st.length(); i++) {
+				char imm = st.charAt(i);
+				if (imm == '(' || imm == '[')
+					arr.push(imm);
+
+				
+				else if (imm == ')') {
+					if (arr.isEmpty()) {
+						System.out.println("no");
+						check = true;
+						break;
+					}
+					else if (arr.pop() == '(')
+						continue;
+					
+					else {
+						System.out.println("no");
+						check = true;
+						break;
+					}
+				}
+				
+				else if (imm == ']') {
+					if (arr.isEmpty()) {
+						System.out.println("no");
+						check = true;
+						break;
+					}
+					
+					else if (arr.pop() == '[')
+						continue;
+					
+					else {
+						System.out.println("no");
+						check = true;
+						break;
+					}
+				}
+			}
+			if(!arr.empty() && !check)
+				System.out.println("no");
+			else if(!check )
+				System.out.println("yes");
+		}
+	
+
+	}
+}
