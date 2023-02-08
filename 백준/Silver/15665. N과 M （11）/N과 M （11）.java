0282bb8e70d102f302a82095b1d95d9ebec2d9bd
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-08 14:30:22 +0900

    [Silver II] Title: N과 M (11), Time: 1544 ms, Memory: 345680 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	static public int N;
+	static public int R;
+	static public HashSet<String> set = new HashSet<>();
+	static public int[] input;
+	static public int[] numbers;
+	static public boolean[] visited;
+	static public StringBuffer out = new StringBuffer();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		R = sc.nextInt();
+		
+		input = new int[N];
+		for(int i = 0; i < N; i++) {
+			input[i] = sc.nextInt();
+		}
+		Arrays.sort(input);
+		numbers = new int[R];
+		visited = new boolean[N + 1];
+		makeNum(0,0);
+		out.delete(out.length()-2, out.length());
+		System.out.println(out.toString());
+	}
+
+	private static void makeNum(int cnt,int start) {
+		// TODO Auto-generated method stub
+		if(cnt == R) {
+			String A = "";
+			for(int i = 0; i < R; i++) {
+				A += (numbers[i] + " ");
+			}
+			if(set.contains(A)) return;
+			set.add(A);
+			out.append(A);
+			out.append("\n");
+			
+			return;
+		}
+		for(int i = 0; i < N; i++) {
+			//if(visited[i])continue;
+			
+			//visited[i] = true;
+			numbers[cnt] = input[i];
+			makeNum(cnt+1, i+1);
+			numbers[cnt] = 0;
+			//visited[i] = false;
+		}
+	}
+}
