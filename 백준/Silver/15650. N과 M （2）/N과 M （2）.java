a8b46b7cfadcd4e91dec541b422548b562846758
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-08 13:46:06 +0900

    [Silver III] Title: N과 M (2), Time: 224 ms, Memory: 18280 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+	static public int N;
+	static public int R;
+	static public int[] numbers;
+	static public boolean[] visited;
+	static public StringBuffer out = new StringBuffer();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		R = sc.nextInt();
+		
+		numbers = new int[R];
+		visited = new boolean[N + 1];
+		makeNum(0, 1 );
+		out.delete(out.length()-2, out.length());
+		System.out.println(out.toString());
+	}
+
+	private static void makeNum(int cnt, int start) {
+		// TODO Auto-generated method stub
+		if(cnt == R) {
+			for(int i = 0; i < R; i++) {
+				out.append(numbers[i] + " ");
+			}
+			out.append("\n");
+			return;
+		}
+		for(int i = start; i <= N; i++) {
+			if(visited[i])continue;
+			visited[i] = true;
+			numbers[cnt] = i;
+			makeNum(cnt+1, i+1);
+			numbers[cnt] = 0;
+			visited[i] = false;
+		}
+	}
+}
