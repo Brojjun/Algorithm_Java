9fd78b60df28240f49adf2e85e0c25fab7eda6f5
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-09 13:25:49 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,410 ms, Memory: 24,116 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class Solution {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int BN = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <=BN; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int maxScore = 0;
+			
+			int[] cal = new int[N];
+			int[] score = new int[N];
+			
+			for(int i = 0; i < N; i++) {
+				StringTokenizer st2 = new StringTokenizer(br.readLine());
+				score[i] = Integer.parseInt(st2.nextToken());
+				cal[i] = Integer.parseInt(st2.nextToken());
+			}
+			
+			for(int i = 0; i < ( 1<<N ); i++) {
+				int imScore = 0;
+				int imCal = 0;
+				for(int j = 0; j < N; j++) {
+					if((i & (1 << j)) == 0) continue;
+						imScore += score[j];
+						imCal += cal[j];
+				}
+				if(imCal <= M)
+					maxScore = Math.max(maxScore, imScore);
+			}
+			
+			System.out.println("#"+t+" "+maxScore);
+		}
+
+		
+
+	}
+}
