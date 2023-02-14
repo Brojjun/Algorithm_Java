686afe7d07efa5e0a9d78481fde01b84557b44d6
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-14 09:41:39 +0900

    [Silver II] Title: 최대 힙, Time: 1060 ms, Memory: 32992 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		PriorityQueue<Integer> arr = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return (o1.intValue() - o2.intValue()) * -1;
+			}
+			
+		});
+		
+		int N = Integer.parseInt(br.readLine());
+	
+		for(int i = 0; i < N; i++) {
+			int n = Integer.parseInt(br.readLine());
+			if(n == 0) {
+				if(arr.isEmpty())System.out.println(0);
+				else
+					System.out.println(arr.poll());
+			}
+			
+			else{
+				arr.add(n);
+			}
+			
+		}
+
+	}
+	
+	
+		
+	
+}
