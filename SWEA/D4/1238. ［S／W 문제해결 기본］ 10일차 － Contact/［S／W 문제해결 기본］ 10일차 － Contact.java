2b451c9d4a52e7d3b026d64dc1d007227620d34c
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-02-28 16:10:11 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 107 ms, Memory: 17,684 KB -BaekjoonHub

---
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int test = 1; test <= 10; test++) 
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+			
+			ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+			
+			for(int i = 0 ; i < 101; i++)arr.add(new ArrayList<Integer>());
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N/2; i++) {
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				 arr.get(from).add(to);
+			}
+			
+			Queue<Integer> que = new LinkedList<Integer>();
+			boolean[] visit = new boolean[101];
+			int big_num = 0;
+			que.add(start);
+			while(!que.isEmpty()) {
+				big_num = -1;
+				ArrayList<Integer> imm = new ArrayList<>();
+				
+				while(!que.isEmpty()) {
+					int now = que.poll();
+					visit[now] = true;
+					imm.add(now);
+				}
+				
+				for(int i = 0; i < imm.size(); i++) {
+					int now = imm.get(i);
+					big_num = Math.max(now, big_num);
+					for(int j = 0; j < arr.get(now).size();j++) {
+						if(visit[arr.get(now).get(j)])continue;
+						que.add(arr.get(now).get(j));
+					}
+				}
+				
+			}
+			
+			System.out.println("#" + test + " "+ big_num);
+			
+			
+		}
+	}
+	
+	
+}
