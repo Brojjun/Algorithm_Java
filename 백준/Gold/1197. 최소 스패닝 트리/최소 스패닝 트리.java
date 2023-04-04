a76aa08b7c45f421a4b85563109e07a5aec4bf7a
Brojjun <122707977+Brojjun@users.noreply.github.com>
2023-04-04 16:52:16 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 616 ms, Memory: 54732 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Point implements Comparable<Point>{
+		int end,value;
+
+		public Point(int end, int value) {
+			super();
+			this.end = end;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			
+			return this.value - o.value;
+		}
+		
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		List<Point>arr[] = new ArrayList[n+1];
+		for(int i = 1; i < n+1; i++)arr[i] = new ArrayList<>();
+	
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int begin = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			arr[begin].add(new Point(end,value));
+			arr[end].add(new Point(begin,value));
+		}
+		boolean[] visit = new boolean[n+1];
+		long min_data = 0;
+		
+		PriorityQueue<Point>pq = new PriorityQueue<>();
+		pq.add(new Point(1,0));
+		
+		while(!pq.isEmpty()) {
+			
+			Point now = pq.poll();
+			if(visit[now.end])continue;
+			visit[now.end] = true;
+			min_data += now.value;
+			
+			for(Point next : arr[now.end]) 
+				if(!visit[next.end]) pq.add(next);
+		}
+		
+		System.out.println(min_data);
+	}
+}
